ab2acf50e2507c9411729006d5e4c21ef0c25e17
Kirill Silianov <admin@siada.org>
2016-04-10 21:36:11 +0500

    Добавлены пункты меню в навигатор

--- app/src/main/java/ru/student_app/student/NavigationActivity.java
+++ app/src/main/java/ru/student_app/student/NavigationActivity.java
@@ -80,17 +80,23 @@ public class NavigationActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_friends) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_pm) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_chat) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_events) {
+
+        } else if (id == R.id.nav_works) {
+
+        }else if (id == R.id.nav_map) {
+
+        }else if (id == R.id.nav_settings) {
+
+        }else if (id == R.id.nav_search) {
 
         }
 
