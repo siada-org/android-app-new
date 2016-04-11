6f29034b62140b804e2e0688e9c42c600a1a94cb
Kirill Silianov <admin@siada.org>
2016-04-11 16:12:15 +0600

    v0.0.1
    - Удалены неиспользуемые иконки пунктов меню проекта;
    - Файл кэша приложения добавлен в игнорируемые
    - Добавлены строковые значения для имени пользователя и статуса в
    заголовке меню
    - Удалена иконка сообщений из шаблона лайоута
    - Удалены неиспользуемы импортируемые классы
    - Обновлены версии компиляторов в Gradle

--- app/src/main/java/ru/student_app/student/NavigationActivity.java
+++ app/src/main/java/ru/student_app/student/NavigationActivity.java
@@ -1,9 +1,6 @@
 package ru.student_app.student;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,25 +9,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -78,6 +66,7 @@ public class NavigationActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
         if (id == R.id.nav_profile) {
